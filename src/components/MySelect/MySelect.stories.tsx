67d08960d7ab97dc01e2b7e5903cdda4afb904e3
Metaloziz <tesla9760614>
2021-12-17 11:28:11 +0300

    StoryBook_15_select_did_80%

--- src/components/MySelect/MySelect.stories.tsx
+++ src/components/MySelect/MySelect.stories.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {MySelect} from "./MySelect";
 import {v1} from "uuid";
 
-
-export default {
-    title: "MySelect",
-    components: MySelect
-}
 export type dataPT = itemDataPT[]
 
 export type itemDataPT = {
     id: string
     title: string
 }
+export type statePT = {
+    title: string
+    setTitle: (title: string) => void
+    collapsed: boolean
+    setCollapsed: (value: boolean) => void
+    hover: string
+    setHover: (value: string) => void
+}
+
+
+export default {
+    title: "MySelect",
+    components: MySelect
+}
 
 const data: dataPT = [
     {id: v1(), title: 'Minsk'},
-    {id: v1(), title: 'LA'},
+    {id: v1(), title: 'LA_+'},
     {id: v1(), title: 'NY'},
 ]
 
-export const MySelectRealisation = () => <MySelect data={data}/>
+
+export const MySelectRealisation = () => {
+
+    const [title, setTitle] = useState<string>(data[0].title)
+    const [collapsed, setCollapsed] = useState<boolean>(true)
+    const [hover, setHover] = useState<string>(title)
+
+    let state: statePT = {
+        title: title,
+        setTitle: setTitle,
+        collapsed: collapsed,
+        setCollapsed: setCollapsed,
+        hover: hover,
+        setHover: setHover
+    }
+
+
+    return <MySelect data={data} state={state}/>
+}
--- src/components/MySelect/MySelect.tsx
+++ src/components/MySelect/MySelect.tsx
@@ -1,34 +1,38 @@
-import React, {useState} from "react";
-import {dataPT} from "./MySelect.stories";
+import React from "react";
+import {dataPT, statePT} from "./MySelect.stories";
 import s from './MySelect.module.css'
 
 
 type MySelectPT = {
     data: dataPT
+    state: statePT
 }
 
-export const MySelect = ({data}: MySelectPT) => {
+export const MySelect = ({data, state}: MySelectPT) => {
 
-
-    const [title, setTitle] = useState<string>(data[0].title)
-    const [collapsed, setCollapsed] = useState<boolean>(true)
 
     const callback1 = () => {
-        setCollapsed(!collapsed)
+        state.setCollapsed(!state.collapsed)
     }
     const callback2 = (title: string) => {
-        setTitle(title)
-        setCollapsed(!collapsed)
+        state.setTitle(title)
+        state.setCollapsed(!state.collapsed)
     }
 
 
     return (
         <div className={s.body}>
-            <div className={s.title} onClick={callback1}>{title}</div>
+            <div className={s.title} onClick={callback1}>{state.title}</div>
             <div className={s.items}>
-                {collapsed && data.map(l => <div  key={l.id} onClick={() => {
-                    callback2(l.title)
-                }}><span>{l.title}</span></div>)}
+                {state.collapsed && data.map(l => <div
+
+                    onMouseEnter={() => {
+                        // state.setTitle(l.title)
+                    }}
+                    className={l.title === state.hover ? s.selected : ''} key={l.id}
+                    onClick={() => {
+                        callback2(l.title)
+                    }}><span>{l.title}</span></div>)}
 
             </div>
 
